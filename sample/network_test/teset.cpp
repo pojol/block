23770ad0f9a9c69f46470f23bb7355aa72ccb44a
DESKTOP-OM379IV\pojol <pojol@outlook.com>
2016-10-26 22:43:37 +0800

    接口该怎么让用户使用呢

--- sample/network_test/teset.cpp
+++ sample/network_test/teset.cpp
@@ -3,9 +3,36 @@
 
 #include <WinSock2.h>
 
+// 接口测试
+
+class Test1 :
+	public gsf::connect_handler,
+	public gsf::session_close_handler
+{
+public :
+	// 连接目标服务器成功
+	virtual void handle_new_connection();
+
+	// 连接断开
+	virtual void handle_close();
+};
+
+
+class Test2 :
+	public gsf::accept_handler,
+	public gsf::session_close_handle
+{
+public:
+	// 有新的session连接到服务器
+	virtual void handle_new_connection();
+
+	// 连接断开
+	virtual void handle_close();
+};
 
 
 int main()
 {
+
 	return 0;
 }
