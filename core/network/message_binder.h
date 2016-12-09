3f4e207bffeea936628e71ff5dd69cb072f968b7
pojol <pojol@outlook.com>
2016-12-09 16:20:46 +0800

    绑定器实装

--- core/network/message_binder.h
+++ core/network/message_binder.h
@@ -11,7 +11,7 @@ namespace gsf
 		template <typename MSG>
 		struct DispatchMsg
 		{
-			typedef void(*DispatchFunc)(void *obj, std::shared_ptr<MSG> &);
+			typedef void(*DispatchFunc)(void *obj, std::shared_ptr<MSG>);
 			DispatchFunc func_;
 			void *obj_;
 
@@ -20,23 +20,37 @@ namespace gsf
 				, obj_(obj)
 			{}
 
-			void dispatch(std::shared_ptr<MSG> &msg)
+			void dispatch(std::shared_ptr<MSG> msg)
 			{
 				func_(obj_, msg);
 			}
 		};
 
+		template<typename MSG, class T,void (T::*func)(std::shared_ptr<MSG>)>
+		inline void DispatchFuncT(void *obj, std::shared_ptr<MSG> msg)
+		{
+			(((T*)obj)->*func)(msg);
+		}
+
+		class Binder
+		{
+		public:
+			virtual void construct_msg(int session_id, ::evbuffer *buf) = 0;
+		};
+
 		template <typename MSG>
-		class MessageBinder
+		class MessageBinder : public Binder
 		{
 		public:
 			static MessageBinder<MSG>& instance();
 			~MessageBinder();
 
-			template <typename T, void(T::*func)(typename MSG::Ptr &)>
+			template <typename T, void(T::*func)(typename MSG::Ptr)>
 			int regist_msg_proc(int msg_id, T *obj);
 
-			int dispatch(typename MSG::Ptr &msg);
+			int dispatch(typename MSG::Ptr msg);
+
+			void construct_msg(int session_id, ::evbuffer *buf);
 
 		private:
 			static MessageBinder<MSG> *instance_;
@@ -62,19 +76,41 @@ namespace gsf
 		}
 
 		template <typename MSG>
-		template<class T, void (T::*func)(typename MSG::Ptr &)>
+		template<class T, void (T::*func)(typename MSG::Ptr)>
 		int MessageBinder<MSG>::regist_msg_proc(int msg_id, T* obj)
 		{
-			DispatchMsg<MSG> dispatcher(func, obj);
-			map_.insert(std::make_pair(msg_id, dispatcher));
+			auto itr = map_.find(msg_id);
+			if (itr == map_.end()){
+				DispatchMsg<MSG> dispatcher(DispatchFuncT<MSG, T, func>, obj);
+				map_.insert(std::make_pair(msg_id, dispatcher));
+				return 0;
+			}
+			else {
+				return -1;
+			}
 		}
 
 		template <typename MSG>
-		int MessageBinder<MSG>::dispatch(typename MSG::Ptr &msg)
+		void MessageBinder<MSG>::construct_msg(int session_id, ::evbuffer *buf)
+		{
+			int len = evbuffer_get_length(buf);
+			char * _buf = (char *)malloc(len);
+			evbuffer_remove(buf, _buf, len);
+
+			std::cout << "thread : " << std::this_thread::get_id() << " session : " << session_id << " recv : " << buf << std::endl;
+
+			evbuffer_free(buf);
+
+			typename MSG::Ptr msg = std::make_shared<MSG>();
+			dispatch(msg);
+		}
+
+		template <typename MSG>
+		int MessageBinder<MSG>::dispatch(typename MSG::Ptr msg)
 		{
 			auto itr = map_.find(msg->get_message_id());
-			if (itr != map_.end){
-				itr->second->dispatch(msg);
+			if (itr != map_.end()){
+				itr->second.dispatch(msg);
 			}
 			else {
 				return -1;
