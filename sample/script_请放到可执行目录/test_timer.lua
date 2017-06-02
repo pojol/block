5962273b413a80607507e015806422affb9d1c9b
huangtao <pojol@outlook.com>
2017-06-02 18:44:32 +0800

    lua module 的timer 测试用例实现， 和c++写法差不多，只是换个语言。

--- sample/script_请放到可执行目录/test_timer.lua
+++ sample/script_请放到可执行目录/test_timer.lua
@@ -1,4 +1,26 @@
 
+
+case_delaymillisecond = {
+	create_timer_ = function(millisecond, cb) end,
+}
+
+function case_delaymillisecond:test()
+
+	local function _on_timer()
+		print_info("case_delaymillisecond", "arrive ! timer id = " .. self.timer_id_)
+	end
+
+	self.timer_f_(2000, function(args) 
+		local _timer_id = args:pop_uint64(0)
+
+		print_info("test_script", "regist success! timer id = " .. tostring(_timer_id))
+
+		self.timer_id_ = _timer_id
+		self.timer_cb_ = _on_timer
+	end)
+end
+
+
 module = {
     before_init = function(dir) end,
 	init = function() end,
@@ -10,6 +32,7 @@ log_m_ = 0
 log_f_ = nil
 
 timer_m_ = 0
+
 
 module.before_init = function(dir)
 	local package_path = {}
@@ -37,7 +60,22 @@ end
 module.init = function()
 	print_info("test_script", "module.init")
 
+	local function _delay_milliseconds(delay, cb)
+		cb_dispatch(timer_m_, eid.timer.delay_milliseconds, module_id, delay, cb)
+	end
+
+	case_delaymillisecond.timer_f_ = _delay_milliseconds
+
+	listen(module_id, eid.timer.timer_arrive, function (args) 
+		local _timer_id = args:pop_uint64(0)
+
+		if _timer_id == case_delaymillisecond.timer_id_ then
+			case_delaymillisecond:timer_cb_()
+		end
+	end)
 	
+
+	case_delaymillisecond:test()
 end
 
 module.execute = function()
