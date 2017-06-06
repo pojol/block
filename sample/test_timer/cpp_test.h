83d6b4144825fc46526d26f167d6712aa6a76b86
DESKTOP-OM379IV\pojol <pojol@outlook.com>
2017-06-06 21:25:59 +0800

    改动下接口使用方式（ 后续要考虑下RAII，和防守型编程的范式。

--- sample/test_timer/cpp_test.h
+++ sample/test_timer/cpp_test.h
@@ -18,70 +18,63 @@
 #include <timer/timer.h>
 #include <log/log.h>
 
+struct TestCaseModule;
+
 struct Case_DelayMilliseconds
 {
 	void test()
 	{
-		create_timer_(2000, [&](gsf::Args args) {
+		module_ptr_->i_delay_milliseconds(2000, [&](gsf::Args args) {
 			timer_id_ = args.pop_uint64(0);
-			log_f_(eid::log::info, "Case_DelayMilliseconds", gsf::Args("regist success! timer id = ", timer_id_));
+			module_ptr_->log_f_(eid::log::info, "Case_DelayMilliseconds", gsf::Args("regist success! timer id = ", timer_id_));
 		});
 	}
 
 	void on_timer()
 	{
-		log_f_(eid::log::info, "Case_DelayMilliseconds", gsf::Args("arrive! timer id = ", timer_id_));
+		module_ptr_->log_f_(eid::log::info, "Case_DelayMilliseconds", gsf::Args("arrive! timer id = ", timer_id_));
 		timer_id_ = gsf::TimerNil;
 	}
 
-	gsf::LogFunc log_f_;
-	std::function<void(int32_t, gsf::CallbackFunc cb)> create_timer_;
-
 	gsf::TimerID timer_id_;
-	
+	TestCaseModule *module_ptr_;
 };
 
 struct Case_DelayDay
 {
 	void test()
 	{
-		create_timer_(6, 0, [&](gsf::Args args) {
+		module_ptr_->i_delay_day(6, 0, [&](gsf::Args args) {
 			timer_id_ = args.pop_uint64(0);
-			log_f_(eid::log::info, "Case_DelayDay", gsf::Args("regist success! timer id = ", args.pop_uint64(0)));
+			module_ptr_->log_f_(eid::log::info, "Case_DelayDay", gsf::Args("regist success! timer id = ", args.pop_uint64(0)));
 		});
 	}
 
 	void on_timer()
 	{
-		log_f_(eid::log::info, "Case_DelayDay", gsf::Args("arrive! timer id = ", timer_id_));
+		module_ptr_->log_f_(eid::log::info, "Case_DelayDay", gsf::Args("arrive! timer id = ", timer_id_));
 		timer_id_ = gsf::TimerNil;
 	}
 
-	gsf::LogFunc log_f_;
-	std::function<void(int32_t, int32_t, gsf::CallbackFunc cb)> create_timer_;
-
 	gsf::TimerID timer_id_;
+	TestCaseModule *module_ptr_;
 };
 
 struct Case_RemoveTimer
 {
-
 	void test()
 	{
-		create_timer_(1000, [&](gsf::Args args) {
+		module_ptr_->i_delay_milliseconds(1000, [&](gsf::Args args) {
 			timer_id_ = args.pop_uint64(0);
-			log_f_(eid::log::info, "Case_RemoveTimer", gsf::Args("regist success! timer id = ", timer_id_));
+			module_ptr_->log_f_(eid::log::info, "Case_RemoveTimer", gsf::Args("regist success! timer id = ", timer_id_));
 		});
 
-		remove_timer_(timer_id_, [&](gsf::Args args) {
-			log_f_(eid::log::info, "Case_RemoveTimer", gsf::Args("remove ret = ", args.pop_int32(0), " timer id = ", timer_id_));
+		module_ptr_->i_remove_timer(timer_id_, [&](gsf::Args args) {
+			module_ptr_->log_f_(eid::log::info, "Case_RemoveTimer", gsf::Args("remove ret = ", args.pop_int32(0), " timer id = ", timer_id_));
 		});
 	}
 
-	gsf::LogFunc log_f_;
-	std::function<void(int32_t, gsf::CallbackFunc cb)> create_timer_;
-	std::function<void(gsf::TimerID, gsf::CallbackFunc cb)> remove_timer_;
-
+	TestCaseModule *module_ptr_;
 	gsf::TimerID timer_id_;
 };
 
@@ -113,25 +106,9 @@ struct TestCaseModule
 
 	void init()
 	{
-		auto _delay_millisecoinds = [&](int32_t delay, gsf::CallbackFunc cb) {
-			dispatch(timer_m_, eid::timer::delay_milliseconds, gsf::Args(get_module_id(), delay), cb);
-		};
-
-		auto _delay_day = [&](int32_t hour, int32_t min, gsf::CallbackFunc cb) {
-			dispatch(timer_m_, eid::timer::delay_day, gsf::Args(get_module_id(), hour, min), cb);
-		};
-
-		auto _remove_timer = [&](uint64_t timer_id, gsf::CallbackFunc cb) {
-			dispatch(timer_m_, eid::timer::remove_timer, gsf::Args(get_module_id(), timer_id), cb);
-		};
-
-		case_delaymillseconds_.create_timer_ = _delay_millisecoinds;
-		case_delaymillseconds_.log_f_ = log_f_;
-		case_delayday_.create_timer_ = _delay_day;
-		case_delayday_.log_f_ = log_f_;
-		case_remove_timer_.create_timer_ = _delay_millisecoinds;
-		case_remove_timer_.remove_timer_ = _remove_timer;
-		case_remove_timer_.log_f_ = log_f_;
+		case_delaymillseconds_.module_ptr_ = this;
+		case_delayday_.module_ptr_ = this;
+		case_remove_timer_.module_ptr_ = this;
 
 		listen(this, eid::timer::timer_arrive, [&](gsf::Args args, gsf::CallbackFunc cb) {
 
@@ -142,6 +119,7 @@ struct TestCaseModule
 			if (_timer_id == case_delayday_.timer_id_) {
 				case_delayday_.on_timer();
 			}
+
 		});
 
 		case_delaymillseconds_.test();
@@ -149,10 +127,26 @@ struct TestCaseModule
 		case_remove_timer_.test();
 	}
 
+	void i_delay_milliseconds(int32_t delay, gsf::CallbackFunc cb)
+	{
+		dispatch(timer_m_, eid::timer::delay_milliseconds, gsf::Args(get_module_id(), delay), cb);
+	}
+
+	void i_delay_day(int32_t hour, int32_t min, gsf::CallbackFunc cb)
+	{
+		dispatch(timer_m_, eid::timer::delay_day, gsf::Args(get_module_id(), hour, min), cb);
+	}
+
+	void i_remove_timer(uint64_t timer_id, gsf::CallbackFunc cb)
+	{
+		dispatch(timer_m_, eid::timer::remove_timer, gsf::Args(get_module_id(), timer_id), cb);
+	}
+
+	gsf::LogFunc log_f_;
+
 private:
 	uint32_t timer_m_;
 	uint32_t log_m_;
-	gsf::LogFunc log_f_;
 
 	Case_DelayMilliseconds case_delaymillseconds_;
 	Case_DelayDay case_delayday_;
