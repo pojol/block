21dfc8f8d22c7232754af9466ec72f0100ab3f4a
pojol <pojol@outlook.com>
2017-12-13 19:53:26 +0800

    bug fix

--- modules/lua_proxy/lua_proxy.h
+++ modules/lua_proxy/lua_proxy.h
@@ -57,9 +57,9 @@ namespace gsf
 			//sol::variadic_args args
 
 			//代理下event的dispatch 和 listen 接口，因为要在这里集中捕获下lua产生的异常。
-			void ldispatch(uint32_t lua_id, uint32_t target, uint32_t event, sol::table &tb, sol::function func);
+			int ldispatch(uint32_t lua_id, uint32_t target, uint32_t event, const sol::table &tb, sol::function func);
 			
-			void llisten(uint32_t lua_id, uint32_t self, uint32_t event, sol::function func);
+			int llisten(uint32_t lua_id, uint32_t self, uint32_t event, sol::function func);
 
 			void create_event(const gsf::ArgsPtr &args, gsf::CallbackFunc callback);
 			void create(uint32_t module_id, std::string dar_name, std::string file_name);
