9313ffc9202b30334ea2b4d4bd3931939ab5329b
pojol <pojol@outlook.com>
2018-07-03 16:29:22 +0800

    删除原有的分布式实现， 新的分布式实现将基于redis进行重构。

--- modules/distributed/coordinate.h
+++ modules/distributed/coordinate.h
@@ -12,30 +12,6 @@ namespace block
 {
 	namespace modules
 	{
-		struct CModuleInfo
-		{
-			block::ModuleID module_id_;
-			std::string module_name_;
-			int32_t module_characteristic_;
-		};
-
-		struct CNodeInfo
-		{
-			std::vector<CModuleInfo> modules;
-
-			int32_t nod_id = 0;
-			std::string type_ = "";
-			int32_t weight_ = 0;
-
-			std::string acceptor_ip_ = "";
-			int32_t acceptor_port_ = 0;
-		};
-		typedef std::shared_ptr<CNodeInfo> NodePtr;
-
-
-		/**
-		coodinator 协调多个相同业务app之间的负载
-		*/
 		class CoodinatorModule
 			: public block::Module
 		{
@@ -50,17 +26,6 @@ namespace block
 			void shut() override;
 
 		private:
-			void eCoordinatAdjustWeight(block::ModuleID target, block::ArgsPtr args);
-			void eCoordinatSelect(block::ModuleID target, block::ArgsPtr args);
-			void eCoordinatRegist(block::ModuleID target, block::ArgsPtr args);
-			void eCoordinatUnregist(block::ModuleID target, block::ArgsPtr args);
-
-			void adjustModuleWeight(int32_t nod_id, const std::string &module_name, block::ModuleID module_id, int32_t characteristic, int32_t weight);
-
-		private:
-
-			std::map<block::SessionID, NodePtr> nodeIDMap_;
-			std::multimap<std::string, NodePtr> nodeNameMap_;
 		};
 	}
 }
--- modules/distributed/node.h
+++ modules/distributed/node.h
@@ -11,36 +11,6 @@ namespace block
 {
 	namespace modules
 	{
-		struct CallbackInfo
-		{
-			block::RpcCallback callback;
-			uint64_t timer_ = 0;
-			uint32_t event_ = 0;
-			int64_t id_ = 0;
-		};
-		typedef std::shared_ptr<CallbackInfo> CallbackPtr;
-
-		struct ModuleInfo
-		{
-			std::string moduleName_ = "";
-			block::ModuleID moduleID_ = block::ModuleNil;
-		};
- 
-		struct NodeInfo
-		{
-			std::vector<ModuleInfo> modules;
-
-			int32_t nodeID_ = 0;
-			std::string nodeType_ = "";
-			std::string nodeIP_ = "";
-			int nodePort_ = 0;
-
-			block::ModuleID connector_m_ = block::ModuleNil;
-
-			int32_t weight_ = 0;
-		};
-		typedef std::shared_ptr<NodeInfo> NodePtr;
-
 		class NodeModule
 			: public block::Module
 		{
@@ -56,29 +26,6 @@ namespace block
 
 		protected:
 
-			void eNodeInit(block::ModuleID target, block::ArgsPtr args);
-			void eNodeRpc(block::ModuleID target, block::ArgsPtr args);
-
-		private:
-			block::SessionID connector_fd_ = block::SessionNil;
-			block::ModuleID connector_m_ = block::ModuleNil;
-			
-			int32_t nodeID_ = 0;
-			std::string nodeType_ = "";
-			std::string nodeIp_ = "";
-			int32_t nodePort_ = 0;
-			std::string rootIP_ = "";
-			int32_t rootPort_ = 0;
-			std::vector<std::pair<block::ModuleID, std::string>> registModules_;
-			
-
-			int32_t rpcDelay_ = 10000;
-
-			std::map<int64_t, CallbackPtr> callbackMap_;
-			std::map<uint64_t, CallbackPtr> timerSet_;
-
-			std::map<int32_t, NodePtr> nodeMap_;
-			std::multimap<std::string, NodePtr> moduleMap_;
 		};
 
 	}
