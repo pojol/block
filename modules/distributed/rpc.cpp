4fe94f9da1a4cbfb876b215423064995eaa987ce
pojol <pojol@outlook.com>
2018-07-09 17:53:06 +0800

    up

--- modules/distributed/rpc.cpp
+++ modules/distributed/rpc.cpp
@@ -56,6 +56,11 @@ void block::modules::RPCModule::init()
 
 void block::modules::RPCModule::execute()
 {
+    // pipeline
+
+    // observer
+
+    // subscriber
 }
 
 void block::modules::RPCModule::shut()
@@ -64,53 +69,71 @@ void block::modules::RPCModule::shut()
 
 bool block::modules::RPCModule::checkConnect()
 {
-    auto replyPtr = command(redis_context_, "PING");
+    auto _replyPtr = command(redis_context_, "PING");
+    int _status = 0;
 
-    if (replyPtr->check("ping")) {
-        return true;
-    }
-    else {
+    do {
+        
+        if (_replyPtr->type != REDIS_REPLY_ERROR){
+            break;
+        }
+
         WARN_LOG("[BLOCK] rpc connect fail! try reconnect!");
-
-        struct timeval _timeout = { 1, timeout_ };
-
         redisFree(redis_context_);
 
+        struct timeval _timeout = { 1, timeout_ };
         redis_context_ = redisConnectWithTimeout(redisIp_.c_str(), redisPort_, _timeout);
         if (redis_context_ && redis_context_->err == 0) {
-            return true;
+            _status = 1;
+            break;
         }
         else {
             if (redis_context_){
                 ERROR_FMTLOG("[BLOCK] rpc service terminated! can't connect! err : {}", redis_context_->err);
+                _status = -1;
             }
             else {
                 ERROR_LOG("[BLOCK] rpc service terminated! can't connect!");
+                _status = -2;
             }
-            return false;
         }
+
+    }while(0);
+
+    freeReplyObject(_replyPtr);
+
+    if (_status < 0){
+        return false;
+    }
+    else {
+        return true;
     }
 }
 
-block::modules::RpcReplyPtr block::modules::RPCModule::command(redisContext *c, const char *format, ...)
+redisReply * block::modules::RPCModule::command(redisContext *c, const char *format, ...)
 {
     va_list ap;
-    void *reply = NULL;
+    void *reply = nullptr;
     va_start(ap, format);
     reply = redisvCommand(c, format, ap);
     va_end(ap);
 
-    return std::make_shared<RpcRedisReply>(static_cast<redisReply*>(reply));
+    return static_cast<redisReply*>(reply);
 }
 
 void block::modules::RPCModule::eSubscription(block::ModuleID target, block::ArgsPtr args)
 {
+    // xrange
 
+    // for xdel
+
+    // transport
 }
 
 void block::modules::RPCModule::eRpc(block::ModuleID target, block::ArgsPtr args)
 {
-    std::string _channelName = args->pop_string();
+    // copy buf
+    std::string _subscription = args->pop_string();
     uint32_t _event = args->pop_ui32();
 
     if (!checkConnect()){
@@ -120,13 +143,32 @@ void block::modules::RPCModule::eRpc(block::ModuleID target, block::ArgsPtr args
 
     std::string _type = args->pop_string();
     uint32_t _page = args->pop_ui32();
+    // push pipeline
 
-    auto _replyPtr = command(redis_context_, "XADD " + _channelName + "* %d %s %d", _event, _type.c_str(), _page);
-    if (!_reply->check("xadd")) {
+    std::string _cmd = "";
+    _cmd += "XADD ";
+    _cmd += _subscription;
+    _cmd += "* %d %s %d";
+    auto _replyPtr = command(redis_context_, _cmd.c_str(), _event, _type.c_str(), _page);   //use args buf
+    if (_replyPtr->type == REDIS_REPLY_ERROR){
         WARN_FMTLOG("[BLOCK] rpc command fail, {}", "xadd");
-        dispatch(target, _event, block::makeArgs(1, false, "disconnect!"));
+        dispatch(target, _event, block::makeArgs(1, false, "command fail!"));
+        freeReplyObject(_replyPtr);
+
         return;
     }
+    freeReplyObject(_replyPtr);
 
-    
+    auto itr = std::find_if(observerVec_.begin(), observerVec_.end(), [&](ObserverVec::value_type it) {
+		return (it.first == std::to_string(target));
+	});
+
+    if (itr != observerVec_.end()){
+        itr->second.push_back("msg_id");
+    }
+    else {
+        std::vector<std::string > _msgs;
+        _msgs.push_back("msg_id");
+        observerVec_.push_back(std::make_pair(std::to_string(target), _msgs));
+    }
 }   
--- modules/distributed/rpc.hpp
+++ modules/distributed/rpc.hpp
@@ -16,53 +16,11 @@ namespace block
 {
 	namespace modules
 	{
-
-        struct RpcRedisReply
-        {
-            RpcRedisReply(redisReply *reply)
-                : reply_(reply) {
-
-            }
-
-            ~RpcRedisReply() {
-                freeReplyObject(reply_);
-            }
-
-            bool check(const std::string &flag) {
-                assert(reply_ && reply_->type != REDIS_REPLY_ERROR);
-
-                if (reply_->type == REDIS_REPLY_ERROR) {
-                    ERROR_FMTLOG("[BLOCK] RedisReply cmd : {} err : {}", flag , reply_->str);
-                    return false;
-                }
-
-                return true;
-            }
-
-            int type() {
-                return reply_->type;
-            }
-
-            int64_t integer() {
-                return reply_->integer;
-            }
-
-            std::string str() {
-                std::string _t(reply_->str, reply_->len);
-                return _t;
-            }
-
-            redisReply * reply_;
-        };
-        typedef std::shared_ptr<RpcRedisReply> RpcReplyPtr;
-
-        /**!
+         /**!
          * 拥塞检查
          * 连接检查
          * rpc 管道|消息 管理
         */
-
-       //! 注： 如果rpc服务单机压力过大，也是可以进行水平分片的。 （但估计这种可能非常小
 
 		class RPCModule
 			: public block::Module
@@ -88,7 +46,10 @@ namespace block
 			**/
 			bool checkConnect();
 
-            RpcReplyPtr command(redisContext *c, const char *format, ...);
+            redisReply * command(redisContext *c, const char *format, ...);
+
+            typedef std::vector<std::pair<std::string, std::vector<std::string>>> ObserverVec;
+            ObserverVec observerVec_;
 
         private:
             int32_t timeout_ = 1000;
